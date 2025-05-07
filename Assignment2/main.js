70e692fc1eb332e7d789789e4e5ac14f3edd2a71
MNguyen3917 <158100650+MNguyen3917@users.noreply.github.com>
2025-05-06 23:44:05 -0400

    Add files via upload

---
+++ Assignment2/main.js
@@ -0,0 +1,22 @@
+if (annyang) {
+  const commands = {
+    'hello': () => {
+      alert('Hello World!');
+    },
+    'change the color to *color': (color) => {
+      document.body.style.backgroundColor = color;
+    },
+    'navigate to *page': (page) => {
+      page = page.toLowerCase();
+      if (page.includes('home')) {
+        window.location.href = 'index.html';
+      } else if (page.includes('stock')) {
+        window.location.href = 'stocks.html';
+      } else if (page.includes('dog')) {
+        window.location.href = 'dogs.html';
+      }
+    }
+  };
+
+  annyang.addCommands(commands);
+}
